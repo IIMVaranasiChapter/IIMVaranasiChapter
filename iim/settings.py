94c943fd1ebd0f5d2a4de3e7d1090e14652025b6
Harsh Paliwal <harsh.paliwal.met16@itbhu.ac.in>
2018-02-14 15:56:25 +0530

    some

--- iim/settings.py
+++ iim/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = 'iimv/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
